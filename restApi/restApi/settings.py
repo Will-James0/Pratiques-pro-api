bc8499eedf5c011d393e198a9938aacde9454d9c
Will-James0 <ndomdimdaniel@gmail.com>
2025-04-28 03:40:19 +0100

    docker config

--- restApi/restApi/settings.py
+++ restApi/restApi/settings.py
@@ -77,6 +77,12 @@ WSGI_APPLICATION = 'restApi.wsgi.application'
 
 DATABASES = {
     'default': {
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': 'postgres',
+        # 'USER': 'postgres',
+        # 'PASSWORD': '1234',
+        # 'HOST': 'db',
+        # 'PORT': '5432',
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
